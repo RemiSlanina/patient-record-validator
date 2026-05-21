e6acca101bb4ca9a53b1f1fe7d343c36d9a3cee3
RemiSlanina <remislanina@gmail.com>
2026-05-21 15:47:38 +0200

    setup tests for json pre validator

---
+++ app/src/test/java/org/example/validation/JsonPreValidatorTest.java
@@ -0,0 +1,34 @@
+package org.example.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonPreValidatorTest {
+    @Test
+    void testInvalidNonNumericStrings(){
+        String json = "[{\"spo2\": \"error\", \"temperature\": \"invalid\"}]";
+
+        List<ValidationIssue> issues = JsonPreValidator.validateJsonTypes(json); 
+        assertEquals(2, issues.size(), "Invalid strings should trigger errors");
+        assertTrue(issues.stream().anyMatch(i -> i.field.equals(("spo2"))));
+        assertTrue(issues.stream().anyMatch(i -> i.field.equals("temperature")));
+    }
+
+    @Test 
+    void testValidNumericString(){
+        String json = "[{\"spo2\": \"98.1\", \"temperature\": \"34.8\"}]";
+        List<ValidationIssue> issues = JsonPreValidator.validateJsonTypes(json); 
+        assertTrue(issues.isEmpty(), "Valid numeric strings shouldn't trigger any errors.");
+    }
+
+    @Test
+    void testMixedValidAndInvald(){
+        String json = "[{\"spo2\": \"99\", \"temperature\": \"thermometer broken\"}]";
+        List<ValidationIssue> issues = JsonPreValidator.validateJsonTypes(json); 
+        assertEquals(1, issues.size(), "Only errors for invalid fields.");
+        assertEquals("temperature", issues.getFirst().field);
+    }
+}
